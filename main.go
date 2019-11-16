e05da5626ffdf31bf747c697a5a5b3d16183b5a5
Karrick S. McDermott <karrick@karrick.net>
2019-11-16 14:42:40 -0500

    print command line options to stdout for --help
    Improved help output

--- main.go
+++ main.go
@@ -133,35 +133,38 @@ func main() {
 	if *optHelp {
 		// Show detailed help then exit, ignoring other possibly conflicting
 		// options when '--help' is given.
-		fmt.Printf(`codenames
+		fmt.Printf(`codenames microservice
 
 Reads ajectives and animals from specified files and on each query returns a
 concatenation from a random choice of one adjective followed by one animal.
 
-SUMMARY:  codenames [options] [file1 [file2 ...]] [options]
+SUMMARY: codenames [options] --adjectives FILE --animals FILE [options]
 
-USAGE: Not all options  may be used with all other  options. See below synopsis
+USAGE: Not all options may be used with all other options. See below synopsis
 for reference.
 
-NOTE: The --certfile and --keyfile must both be provided to serve HTTPS. When serving HTTPS
+NOTE: The --certfile and --keyfile must both be provided to serve HTTPS. When
+serving HTTPS, the --redirect command line option causes HTTP traffic to be
+redirected to HTTPS port.
 
 SYNOPSIS:
 
     codenames [--quiet | [--force | --verbose]]
+              [--http NUMBER]
+              [--logs FILE]
+              [--certfile FILE] [--keyfile FILE] [--https NUMBER] [--redirect]
               --adjectives FILE --animals FILE
-              [--http NUMBER] [--https NUMBER]
-              [--certfile FILE] [--keyfile FILE] [--redirect]
 
 EXAMPLES:
 
     codenames --adjectives adjectives.txt --animals animals.txt
-    codenames --adjectives adjectives.txt --animals animals.txt --port 8080
-    codenames --adjectives adjectives.txt --animals animals.txt --
-    codenames --adjectives adjectives.txt --animals animals.txt --certfile $HOME/.local/share/mkcert/rootCA.pem --keyfile $HOME/.local/share/mkcert/rootCA-key.pem --redirect
+    codenames --adjectives adjectives.txt --animals animals.txt --http 80
+    codenames --adjectives adjectives.txt --animals animals.txt --certfile $HOME/.local/share/mkcert/rootCA.pem --keyfile $HOME/.local/share/mkcert/rootCA-key.pem
+    codenames --adjectives adjectives.txt --animals animals.txt --http 80 --https 443 --certfile $HOME/.local/share/mkcert/rootCA.pem --keyfile $HOME/.local/share/mkcert/rootCA-key.pem --redirect
 
 Command line options:
 `)
-		golf.PrintDefaults() // frustratingly, this only prints to stderr, and cannot change because it mimicks flag stdlib package
+		golf.PrintDefaultsTo(os.Stdout)
 		return
 	}
 
